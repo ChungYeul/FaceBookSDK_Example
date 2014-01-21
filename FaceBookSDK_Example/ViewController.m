5a2977bb0f608674e332c72d25cf4db71ce6170a
ChungYeul In <sldk86@naver.com>
2014-01-21 17:52:29 +0900

    FaceBookSDK를 이용하여 친구목록 받아오기입니다.
    FaceBookSDK를 이용하여 친구목록 받아오기입니다.

--- FaceBookSDK_Example/ViewController.m
+++ FaceBookSDK_Example/ViewController.m
@@ -7,17 +7,55 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import <FacebookSDK/FacebookSDK.h>
+@interface ViewController ()<FBLoginViewDelegate, UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *table;
+@property (strong, nonatomic) NSMutableArray *data;
 @end
 
 @implementation ViewController
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FRIEND_CELL" forIndexPath:indexPath];
+    NSDictionary *one = self.data[indexPath.row];
+    cell.textLabel.text = one[@"name"];
+    return cell;
+}
+
+- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+    [FBRequestConnection startWithGraphPath:@"me/friends"
+                          completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                              if (!error) {
+                                  self.data = result[@"data"];
+                                  // 메인 쓰레드에서 UI 업데이트
+                                  [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                                      [self.table reloadData];
+                                  }];
+                              } else {
+                                  // An error occurred, we need to handle the error
+                                  // See: https://developers.facebook.com/docs/ios/errors
+                              }
+                          }];
+}
+
+- (void)loginViewShowingLoggedOutUser:(FBLoginView *)loginView {
+    NSLog(@"logout");
+    [self.data removeAllObjects];
+    [self.table reloadData];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    FBLoginView *loginView = [[FBLoginView alloc] init];
+    // Align the button in the center horizontally
+    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 50);
+    loginView.readPermissions = @[@"basic_info", @"email", @"user_likes"];
+    loginView.delegate = self;
+    [self.view addSubview:loginView];
 }
 
 - (void)didReceiveMemoryWarning
